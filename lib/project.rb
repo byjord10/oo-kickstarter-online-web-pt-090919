d3fdfe779a9f401aea88babba33b7e8c60a21cee
Brianna Jordan <bryvjordan9@gmail.com>
2019-10-10 16:24:06 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,9 @@
-class Project 
-  attr_reader :title, :backers 
+class Project
+  attr_reader :title, :backers
 
   def initialize(title)
-    @title = title 
-    backers = [] 
-  end 
+    @title = title
+    backers = []
+  end
 
 end 
