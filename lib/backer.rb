3e8c1336b1beba875f37b6893b3aee088ec49034
Brianna Jordan <bryvjordan9@gmail.com>
2019-10-10 16:20:27 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,13 +1,13 @@
 class Becker
-  attr_reader :name, :backed_projects 
+  attr_reader :name, :backed_projects
 
   def initialize(name)
-    @name = name 
+    @name = name
     @back_projects = []
-  end 
+  end
 
   def back_project(project)
-    backed_projects << project 
-    project.backers << self 
-  end 
+    backed_projects << project
+    project.backers << self
+  end
 end 
